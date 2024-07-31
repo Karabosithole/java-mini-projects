9c93e43d8ee49605128f9e5ac1e0ce764ba3d2e7
Karabosithole <sitholekarabo0@gmail.com>
2024-07-31 18:10:29 +0200

    need to fix tictactoe bug

--- src/main/java/com.practice/die/sumDice/DiceGame.java
+++ src/main/java/com.practice/die/sumDice/DiceGame.java
@@ -4,9 +4,9 @@ import java.util.Random;
 import java.util.stream.IntStream;
 
 public class DiceGame {
-    public int RollDice;{
-        Random random = new Random();
-        IntStream ints = IntStream.of(random.nextInt(1,7));
-
-    }
+//    public int RollDice;{
+//        Random random = new Random();
+//        IntStream ints = IntStream.of(random.nextInt(1,7));
+//
+//    }
 }
