3a0cd12e54fb73989aa6980c9db757d33abad49a
Karabosithole <sitholekarabo0@gmail.com>
2024-07-31 19:32:14 +0200

    Added fibonacci sequence, recursive and other

--- src/main/java/com.practice/fibonacci/Recursive.java
+++ src/main/java/com.practice/fibonacci/Recursive.java
@@ -1,1 +1,30 @@
 package com.practice.fibonacci;
+
+import java.io.*;
+
+class Recursive {
+
+    // Function to print the fibonacci series
+    static int fib(int n)
+    {
+        // Base Case
+        if (n <= 1)
+            return n;
+
+        // Recursive call
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    // Driver Code
+    public static void main(String args[])
+    {
+        // Given Number N
+        int N = 10;
+
+        // Print the first N numbers
+        for (int i = 0; i < N; i++) {
+
+            System.out.print(fib(i) + " ");
+        }
+    }
+}
---
+++ src/main/java/com.practice/tinyProjects/primes/Primes.java
@@ -0,0 +1,14 @@
+package com.practice.tinyProjects.primes;
+
+import java.util.Scanner;
+
+public class Primes {
+    Scanner newObject = new Scanner(System.in);
+    String userInput = newObject.nextLine();
+
+
+    Primes(){
+
+    }
+
+}
