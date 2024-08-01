ac123926e71597e52fb6005c8c8314164d36fe6f
Karabosithole <sitholekarabo0@gmail.com>
2024-08-01 15:51:49 +0200

    Added collatz instructions

--- src/main/java/com.practice/evenAndOdd/Collatz.java
+++ src/main/java/com.practice/evenAndOdd/Collatz.java
@@ -1,1 +1,11 @@
 package com.practice.evenAndOdd;
+
+public class Collatz {
+
+    /**Starting with any positive integer N, Collatz sequence is defined corresponding to n as the numbers formed by the following operations :
+
+    *If n is even, then n = n / 2.
+    *If n is odd, then n = 3*n + 1.
+    *Repeat above steps, until it becomes 1.
+     * */
+}
