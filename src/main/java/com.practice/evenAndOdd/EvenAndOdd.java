42cb9c6bd01f7743b36c7e0a5a304bb0f7743d53
Karabosithole <sitholekarabo0@gmail.com>
2024-08-01 15:47:17 +0200

    simple even and odd caulculation

--- src/main/java/com.practice/evenAndOdd/EvenAndOdd.java
+++ src/main/java/com.practice/evenAndOdd/EvenAndOdd.java
@@ -1,1 +1,20 @@
 package com.practice.evenAndOdd;
+
+public class EvenAndOdd {
+
+
+    static void EvenNumber(int N)
+    {
+        if (N%2 == 0);{
+        System.out.println("This is an even number");
+        }
+    }
+    // Driver Code
+    public static void main(String args[])
+    {
+        // Given Number N
+        int N = 10;
+        // Function Call
+        EvenNumber(N);
+    }
+}
