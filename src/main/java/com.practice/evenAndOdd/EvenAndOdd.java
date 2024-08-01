74904184dc8ca8f7292e199755e599f9a403ce02
Karabosithole <sitholekarabo0@gmail.com>
2024-08-01 16:33:39 +0200

    Added condition to even or odd checks

--- src/main/java/com.practice/evenAndOdd/EvenAndOdd.java
+++ src/main/java/com.practice/evenAndOdd/EvenAndOdd.java
@@ -1,20 +1,33 @@
 package com.practice.evenAndOdd;
 
+import java.util.Scanner;
+
 public class EvenAndOdd {
 
 
     static void EvenNumber(int N)
     {
-        if (N%2 == 0);{
+        if (N%2 == 0){
         System.out.println("This is an even number");
         }
+        else{
+            System.out.println("This is an odd number");
+
+    }
     }
     // Driver Code
     public static void main(String args[])
     {
-        // Given Number N
-        int N = 10;
-        // Function Call
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int N = scanner.nextInt();
+
         EvenNumber(N);
+
+        scanner.close();
+        // Given Number N
+//        int N = 10;
+        // Function Call
+//        EvenNumber(N);
     }
 }
