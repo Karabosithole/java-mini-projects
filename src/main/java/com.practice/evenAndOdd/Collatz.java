c74926c8cd1d7c57ac9017253cff998ea5eba7a3
Karabosithole <sitholekarabo0@gmail.com>
2024-08-02 10:55:55 +0200

    Collatz setup

--- src/main/java/com.practice/evenAndOdd/Collatz.java
+++ src/main/java/com.practice/evenAndOdd/Collatz.java
@@ -1,11 +1,32 @@
 package com.practice.evenAndOdd;
 
+import java.util.Scanner;
+
+/**Starting with any positive integer N,
+ * Collatz sequence is defined corresponding
+ * to n as the numbers formed by the following operations :
+
+ *If n is even, then n = n / 2.
+ *If n is odd, then n = 3*n + 1.
+ *Repeat above steps, until it becomes 1.
+ * */
+
 public class Collatz {
 
-    /**Starting with any positive integer N, Collatz sequence is defined corresponding to n as the numbers formed by the following operations :
+    static void NumberCheck (int N){
 
-    *If n is even, then n = n / 2.
-    *If n is odd, then n = 3*n + 1.
-    *Repeat above steps, until it becomes 1.
-     * */
+
+
+    }
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int N = scanner.nextInt();
+
+        NumberCheck(N);
+
+
+
+    }
 }
