7e8b33467a1158a9acead6753947f603ed2ac733
Karabosithole <sitholekarabo0@gmail.com>
2024-08-02 16:55:56 +0200

    need to finish the collatz recursive method

--- src/main/java/com.practice/evenAndOdd/CollatzRecursive.java
+++ src/main/java/com.practice/evenAndOdd/CollatzRecursive.java
@@ -1,10 +1,21 @@
 package com.practice.evenAndOdd;
 
 import java.util.Scanner;
+/**Starting with any positive integer N,
+ * Collatz sequence is defined corresponding
+ * to n as the numbers formed by the following operations :
 
+ *If n is even, then n = n / 2.
+ *If n is odd, then n = 3*n + 1.
+ *Repeat above steps, until it becomes 1.
+ * */
 public class CollatzRecursive {
 
-    static void CheckNumbers (){
+    static void CheckNumbers (int N){
+        System.out.println(N);
+        if (N == 1) {
+            System.out.print(", "+N);
+        }
 
 
     }
@@ -15,6 +26,8 @@ public class CollatzRecursive {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int N = scanner.nextInt();
+        CheckNumbers(N);
+        scanner.close();
 
 
     }
