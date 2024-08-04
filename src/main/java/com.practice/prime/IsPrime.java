fd60e383b6f3021d1c77cf17defef62ef3cfe335
Karabosithole <sitholekarabo0@gmail.com>
2024-08-04 16:39:44 +0200

    Added explanation on what is a prime number

--- src/main/java/com.practice/prime/IsPrime.java
+++ src/main/java/com.practice/prime/IsPrime.java
@@ -2,6 +2,11 @@ package com.practice.prime;
 
 import java.util.Scanner;
 
+/** prime numbers, not divisable by any number a whole number greater than 1
+ * that cannot be exactly divided by any whole number other than itself
+ * and 1 (e.g. 2, 3, 5, 7, 11).
+*/
+
 public class IsPrime {
    public static void primeNumberCheck(int num){
 
