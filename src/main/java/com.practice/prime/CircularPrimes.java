ed3fb2638d15804c56996a8225c8266253624f2f
Karabosithole <sitholekarabo0@gmail.com>
2024-08-04 17:12:04 +0200

    Added info for more primes practice

--- src/main/java/com.practice/prime/CircularPrimes.java
+++ src/main/java/com.practice/prime/CircularPrimes.java
@@ -1,1 +1,16 @@
 package com.practice.prime;
+
+/**
+ * Circular Prime : A circular prime is a prime number with the property
+ * that the number generated at each intermediate step when cyclically permuting its
+ * (base 10) digits will be prime.
+ *
+ * For example, 1193 is a circular prime,
+ * since 1931, 9311 and 3119 all are also prime.
+ * A circular prime with at least two digits can only consist of combinations of the digits
+ * 1, 3, 7 or 9, because having 0, 2, 4, 6 or 8 as the last digit makes the number divisible by 2,
+ * and having 0 or 5 as the last digit makes it divisible by 5.
+ */
+
+public class CircularPrimes {
+}
--- src/main/java/com.practice/prime/GoldbachConjecture.java
+++ src/main/java/com.practice/prime/GoldbachConjecture.java
@@ -1,1 +1,7 @@
 package com.practice.prime;
+/**
+ *all prime numbers that are greater than 2 can be expressed as the sum of 2 odd prime numbers.
+*/
+
+public class GoldbachConjecture {
+}
