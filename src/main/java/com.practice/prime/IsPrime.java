66bad30d013b554494e98f99d3433fc4657c0031
Karabosithole <sitholekarabo0@gmail.com>
2024-08-04 16:30:51 +0200

    Setting up primary numbers

--- src/main/java/com.practice/prime/IsPrime.java
+++ src/main/java/com.practice/prime/IsPrime.java
@@ -1,1 +1,20 @@
 package com.practice.prime;
+
+import java.util.Scanner;
+
+public class IsPrime {
+   public static void primeNumberCheck(int num){
+
+    }
+
+
+    static void main(String args []){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number");
+        int num = scanner.nextInt();
+
+       primeNumberCheck(num);
+       scanner.close();
+
+    }
+}
