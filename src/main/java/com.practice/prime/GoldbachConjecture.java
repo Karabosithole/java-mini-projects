8a89f90ec2917e06646598e0dc0d74a300b5dd2a
Karabosithole <sitholekarabo0@gmail.com>
2024-08-05 15:19:01 +0200

    Setting up goldbachconjecture

--- src/main/java/com.practice/prime/GoldbachConjecture.java
+++ src/main/java/com.practice/prime/GoldbachConjecture.java
@@ -1,7 +1,25 @@
 package com.practice.prime;
+
+import java.util.Scanner;
+
 /**
  *all prime numbers that are greater than 2 can be expressed as the sum of 2 odd prime numbers.
 */
 
 public class GoldbachConjecture {
+    public static void IsItPrime(int num){
+
+    }
+
+    static void main (String ages []){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please input a number ");
+        int num = scanner.nextInt();
+
+        IsItPrime(num);
+
+        scanner.close();
+
+
+    }
 }
