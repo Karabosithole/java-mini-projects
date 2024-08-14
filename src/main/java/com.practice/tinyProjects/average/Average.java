9610ac7e5009b55b5f3dd1af8e99a8f50049dfc9
Karabosithole <sitholekarabo0@gmail.com>
2024-08-14 18:13:40 +0200

    more additions to Average

--- src/main/java/com.practice/tinyProjects/average/Average.java
+++ src/main/java/com.practice/tinyProjects/average/Average.java
@@ -20,6 +20,9 @@ public class Average {
         System.out.println("Please enter the next number ");
         double num3 = scanner.nextDouble();
 
+        // Close the scanner
+        scanner.close();
+
         //calculate the sum of the numbers
         double sum = num1 +num2 +num3;
         //define how many numbers there are
