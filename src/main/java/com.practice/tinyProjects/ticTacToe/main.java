240ade53f8163e3471603f5686c9389a2a8f0e4f
Karabosithole <sitholekarabo0@gmail.com>
2024-08-14 19:01:56 +0200

    add to tiktaktoe and added comments of what i need for improvments

--- src/main/java/com.practice/tinyProjects/ticTacToe/main.java
+++ src/main/java/com.practice/tinyProjects/ticTacToe/main.java
@@ -4,5 +4,7 @@ public class main {
     public static void main (String[]args){
         TicTacToe tictactoe = new TicTacToe(); // creating an instance of the tictactoe class (step 1)
     }
-
+//Todo add option if you wanna start a new game
+    //Todo add a game number counter
+    //todo add when no one wins
 }
