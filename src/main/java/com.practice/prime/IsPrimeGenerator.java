5637c1fcda3b959aa34b45b91ee648d518569c3c
Karabosithole <sitholekarabo0@gmail.com>
2024-08-26 18:37:29 +0200

    added a method to primeGenerator

--- src/main/java/com.practice/prime/IsPrimeGenerator.java
+++ src/main/java/com.practice/prime/IsPrimeGenerator.java
@@ -1,16 +1,22 @@
 package com.practice.prime;
 
+import java.util.Scanner;
+
 public class IsPrimeGenerator {
 
-    public void numberChecker(){
+    public void numberChecker(int num){
         boolean isPrime = true;
+
 
     }
 
 
 
     public void main (String args[]){
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int num = scanner.nextInt();
+        numberChecker(num);
 
     }
 }
