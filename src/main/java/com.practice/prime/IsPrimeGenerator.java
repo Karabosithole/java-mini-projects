0a56fe8b3f0d68eeeb15bb97754104d061bf8c0a
Karabosithole <sitholekarabo0@gmail.com>
2024-08-26 18:25:16 +0200

    prime number generator practice

--- src/main/java/com.practice/prime/IsPrimeGenerator.java
+++ src/main/java/com.practice/prime/IsPrimeGenerator.java
@@ -1,4 +1,16 @@
 package com.practice.prime;
 
 public class IsPrimeGenerator {
+
+    public void numberChecker(){
+        boolean isPrime = true;
+
+    }
+
+
+
+    public void main (String args[]){
+
+
+    }
 }
