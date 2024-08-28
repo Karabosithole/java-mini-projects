c20c73ef1ee965c4261eb4d90f56c40b538a5eaf
Karabosithole <sitholekarabo0@gmail.com>
2024-08-28 16:41:48 +0200

    corrected logic for is prime

--- src/main/java/com.practice/prime/IsPrimeGenerator.java
+++ src/main/java/com.practice/prime/IsPrimeGenerator.java
@@ -10,19 +10,28 @@ import java.util.Scanner;
 
 public class IsPrimeGenerator {
 
-    public static void numberChecker(int num){
+    public static void numberChecker(int num) {
         boolean isPrime = true;
-        for (int i=2;i<= Math.sqrt(num);i++){
-            if(num%i == 0 )
 
+        if (num <= 1) {
+            System.out.println(num + " is not prime");
+            return;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
                 isPrime = false;
+                break;
+            }
+        }
 
-            else {isPrime = true;
-            break;}
+        if (isPrime) {
+            System.out.println(num + " is a prime number");
+        } else {
+            System.out.println(num + " is not prime");
+        }
     }
 
-
-    }
 
 
 
