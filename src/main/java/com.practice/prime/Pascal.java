ae424c0a560e6321825fa120bd76eb45ea6731e7
Karabosithole <sitholekarabo0@gmail.com>
2024-09-12 17:13:37 +0200

    Pascals triangle doc string

--- src/main/java/com.practice/prime/Pascal.java
+++ src/main/java/com.practice/prime/Pascal.java
@@ -1,4 +1,13 @@
 package com.practice.prime;
 
+/**The number of entries in every line is equal to line number.
+ * For example, the first line has “1“, the second line has “1 1“,
+ * the third line has “1 2 1“,.. and so on.
+ * Every entry in a line is value of a Binomial Coefficient.
+ * The value of ith entry in line number line is C(line, i).
+ * The value can be calculated using following formula.
+ * C(line, i) = line! / ( (line-i)! * i! )
+ */
+
 public class Pascal {
 }
--- src/main/java/com.practice/prime/Recursive.java
+++ src/main/java/com.practice/prime/Recursive.java
@@ -1,3 +1,62 @@
 package com.practice.prime;
+import java.util.Scanner;
 
-public class Recursive {}
+
+public class Recursive {
+
+    /**
+     * Recursively checks whether a number is prime.
+     * <p>
+     * A prime number is a number greater than 1 that is only divisible by 1 and itself.
+     * This function checks divisibility by decrementing the divisor until it reaches 2.
+     * If no divisors are found that evenly divide the number, the number is prime.
+     *
+     * @param n       The number to check for primality.
+     * @param divisor The divisor to check divisibility of n. Initially, this should be set to n - 1.
+     * @return True if the number is prime, False otherwise.
+     * <p>
+     * Example usage:
+     * <pre>
+     * {@code
+     * PrimeChecker.isPrime(7, 6);  // returns true
+     * PrimeChecker.isPrime(10, 9); // returns false
+     * }
+     * </pre>
+     */
+    public static class PrimeChecker {
+        public static boolean isPrime(int n, int divisor) {
+            if (n < 2) {
+                return false;
+            }
+            if (divisor == 1) {
+                return true;
+            }
+            if (n % divisor == 0) {
+                return false;
+            }
+            return isPrime(n, divisor - 1);
+        }
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter a number: ");
+            int n = scanner.nextInt();
+
+            // Set the divisor as n - 1
+            int divisor = n - 1;
+
+            // Check if the number is prime and print the result
+            boolean result = isPrime(n, divisor);
+            if (result) {
+                System.out.println(n + " is a prime number.");
+            } else {
+                System.out.println(n + " is not a prime number.");
+            }
+
+            scanner.close();
+        }
+
+    }
+}
+
