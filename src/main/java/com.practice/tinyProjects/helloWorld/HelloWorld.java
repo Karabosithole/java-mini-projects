ce644e078702e33ba477718f3b7dc888f2883c83
Karabosithole <sitholekarabo0@gmail.com>
2024-09-14 10:02:02 +0200

    corrected the call of Hello world in mane, so it properly calls the hellow world method

--- src/main/java/com.practice/tinyProjects/helloWorld/HelloWorld.java
+++ src/main/java/com.practice/tinyProjects/helloWorld/HelloWorld.java
@@ -1,13 +1,22 @@
 package com.practice.tinyProjects.helloWorld;
 
 public class HelloWorld {
-        public static void main(String[] args) {
-                System.out.println("Hello world");
-        }
+
+
 
         public void printHelloWorld() {
                 System.out.println("Hello world");
         }
+
+        public static void main(String[] args) {
+
+                HelloWorld obj = new HelloWorld();  // Create an instance of MyClass
+                obj.printHelloWorld();        // Call the instance method
+//                System.out.println("Hello world");
+        }
+
+
+
 }
 
 
