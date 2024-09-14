8cd91ebff617ef66391cc63f2b318cd5fbe77d13
Karabosithole <sitholekarabo0@gmail.com>
2024-09-14 12:43:29 +0200

    Added doc strings for fib sequence

---
+++ src/main/java/com.practice/tinyProjects/fibonacci/Fibonacci.java
@@ -0,0 +1,23 @@
+package com.practice.tinyProjects.fibonacci;
+
+/**
+ * This method calculates the nth number in the Fibonacci sequence.
+ *
+ * The Fibonacci sequence is a series of numbers in which each number
+ * (Fibonacci number) is the sum of the two preceding ones. It starts
+ * with 0 and 1.
+ *
+ * Example of the Fibonacci sequence:
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+ *
+ * The formula is:
+ * F(n) = F(n-1) + F(n-2) for n >= 2, where:
+ * - F(0) = 0
+ * - F(1) = 1
+ *
+ * @param n The position in the Fibonacci sequence to calculate (non-negative).
+ * @return The Fibonacci number at the nth position.
+ */
+
+public class Fibonacci {
+}
