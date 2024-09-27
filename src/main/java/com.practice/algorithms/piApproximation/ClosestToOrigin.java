a05e46c75922322db0770e1f818c85ac9497b848
Karabosithole <sitholekarabo0@gmail.com>
2024-09-27 18:03:01 +0200

    added more doc strings

--- src/main/java/com.practice/algorithms/piApproximation/ClosestToOrigin.java
+++ src/main/java/com.practice/algorithms/piApproximation/ClosestToOrigin.java
@@ -1,1 +1,19 @@
 package com.practice.algorithms.piApproximation;
+
+/**
+ * Finds the point closest to the origin (0, 0) from a given list of points.
+ *
+ * Each point is represented by its (x, y) coordinates in a 2D plane. The method
+ * calculates the distance of each point from the origin using the Euclidean
+ * distance formula: distance = √(x² + y²). The point with the smallest distance
+ * to the origin is returned.
+ *
+ * @param points an array of 2D points, where each point is represented as an
+ *               array with two elements: [x, y]
+ * @return the point closest to the origin, represented as an array [x, y]
+ * @throws IllegalArgumentException if the points array is null or empty
+ */
+
+
+public class ClosestToOrigin {
+}
