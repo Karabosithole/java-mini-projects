69cbcf8d11cb587008e00466938051e0515f8c86
Karabosithole <sitholekarabo0@gmail.com>
2024-10-04 19:16:37 +0200

    Did counting down from 10

--- src/main/java/com.practice/basics/countFromTen/Count.java
+++ src/main/java/com.practice/basics/countFromTen/Count.java
@@ -1,8 +1,15 @@
 package com.practice.basics.countFromTen;
 
 public class Count {
-    void main(String [] args){
+    void main(String[] args) {
+        for (int i = 10; i >= 0; i--) {
+            if (i >= 1) {
+                System.out.print(i + ", ");
+            } else {
+                System.out.print(i);
+            }
 
 
+        }
     }
 }
